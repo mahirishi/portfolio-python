6cf40ce77f2b876fe3261733c604e7a93177f0a8
Hafiz Furqan Irfan <43038504+leofurqan@users.noreply.github.com>
2024-08-15 17:22:27 +0500

    initial code

---
+++ portfolio/views.py
@@ -0,0 +1,49 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+services = [
+    {
+        'icon': 'python.png',
+        'title': 'Web Development',
+        'description': 'We provide web development services in Python (Django), MERN Stack'
+    },
+    {
+        'icon': 'android.png',
+        'title': 'Mobile Development',
+        'description': 'We provide mobile app development services in full stack android JAVA, Kotlin and Google Material'
+    },
+    {
+        'icon': 'photoshop.png',
+        'title': 'Graphic Designing',
+        'description': 'One stop solution for all your graphic design needs'
+    },
+    {
+        'icon': 'photoshop.png',
+        'title': 'Graphic Designing',
+        'description': 'One stop solution for all your graphic design needs'
+    },
+    {
+        'icon': 'photoshop.png',
+        'title': 'Graphic Designing',
+        'description': 'One stop solution for all your graphic design needs'
+    },
+    {
+        'icon': 'photoshop.png',
+        'title': 'Graphic Designing',
+        'description': 'One stop solution for all your graphic design needs'
+    }
+]
+
+def home(request):
+    context = {
+        'services': services
+    }
+    return render(request, 'portfolio/home.html', context)
+
+def about(request):
+    return render(request, 'portfolio/about.html')
+
+def contact(request):
+    return HttpResponse('<h1>My Contact Page</h1>')
