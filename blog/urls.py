34a929918512a35b49783f94734491d8b1514640
Hafiz Furqan Irfan <43038504+leofurqan@users.noreply.github.com>
2024-08-19 17:14:41 +0500

    changes

--- blog/urls.py
+++ blog/urls.py
@@ -2,8 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='portfolio-home'),
-    path('about/', views.about, name='portfolio-about'),
-    path('services/', views.services, name='portfolio-services'),
-    path('contact/', views.contact, name='portfolio-contact')
+    path('', views.home, name='blog-home')
 ]
