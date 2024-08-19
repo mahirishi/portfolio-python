82404eecc37933b17ba08d0a2baceda97dc33378
Hafiz Furqan Irfan <43038504+leofurqan@users.noreply.github.com>
2024-08-19 16:45:32 +0500

    blogs

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='portfolio-home'),
     path('about/', views.about, name='portfolio-about'),
+    path('services/', views.services, name='portfolio-services'),
     path('contact/', views.contact, name='portfolio-contact')
 ]
--- portfolio/views.py
+++ portfolio/views.py
@@ -3,50 +3,61 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-services = [
+services_data = [
     {
         'icon': 'python.png',
         'title': 'Web Development',
         'description': 'We provide web development services in Python (Django), MERN Stack'
     },
-    {
-        'icon': 'android.png',
-        'title': 'Mobile Development',
-        'description': 'We provide mobile app development services in full stack android JAVA, Kotlin and Google Material'
-    },
-    {
-        'icon': 'photoshop.png',
-        'title': 'Graphic Designing',
-        'description': 'One stop solution for all your graphic design needs'
-    },
-    {
-        'icon': 'photoshop.png',
-        'title': 'Graphic Designing',
-        'description': 'One stop solution for all your graphic design needs'
-    },
-    {
-        'icon': 'photoshop.png',
-        'title': 'Graphic Designing',
-        'description': 'One stop solution for all your graphic design needs'
-    },
-    {
-        'icon': 'photoshop.png',
-        'title': 'Graphic Designing',
-        'description': 'One stop solution for all your graphic design needs'
-    }
+    # {
+    #     'icon': 'android.png',
+    #     'title': 'Mobile Development',
+    #     'description': 'We provide mobile app development services in full stack android JAVA, Kotlin and Google Material'
+    # },
+    # {
+    #     'icon': 'photoshop.png',
+    #     'title': 'Graphic Designing',
+    #     'description': 'One stop solution for all your graphic design needs'
+    # },
+    # {
+    #     'icon': 'photoshop.png',
+    #     'title': 'Graphic Designing',
+    #     'description': 'One stop solution for all your graphic design needs'
+    # },
+    # {
+    #     'icon': 'photoshop.png',
+    #     'title': 'Graphic Designing',
+    #     'description': 'One stop solution for all your graphic design needs'
+    # },
+    # {
+    #     'icon': 'photoshop.png',
+    #     'title': 'Graphic Designing',
+    #     'description': 'One stop solution for all your graphic design needs'
+    # }
 ]
 
 def home(request):
     context = {
-        'services': services
+        'title': 'Home Page',
+        'about': """ I am a full stack developer having 6+ years of experience. My area of expertise are HTML, CSS, JS, PHP, Laravel, MySQL, MongoDB, React, Node,
+            Express, Android, Java, Kotlin, Python, Django""",
+        'services': services_data
     }
     return render(request, 'portfolio/home.html', context)
 
 def about(request):
     context = {
-        'services': services
+        'title': 'About Page',
+        'services': services_data
     }
     return render(request, 'portfolio/about.html', context)
 
+def services(request):
+    context = {
+        'title': 'Services Page',
+        'services': services_data
+    }
+    return render(request, 'portfolio/services.html', context)
+
 def contact(request):
-    return HttpResponse('<h1>My Contact Page</h1>')
+    return render(request, 'portfolio/contact.html')
